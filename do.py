c1690c8899df5012e8cd04bf1ac3c492f49bcc88
petr57 <ololo@gmail.com>
2015-05-19 21:05:05 +0300

    add aggregation to cmd

--- do.py
+++ do.py
@@ -5,9 +5,13 @@ import db_delete as db_del
 import db_add as db_add
 import db_init as db_init
 import db_generate as db_gen
+import db_seasonTeam as db_seas_team
+import db_seasonStat as db_seas_stat
+import db_seasonPlayer as db_seas_player
 
 def print_help():
 	print('{init|help|gen}')
+	print('season {player|stat|team} value')
 	print('{add|del} object param1 param2 ...')
 	print('add season datebegin dateend count')
 	print('add rule season place points')
@@ -53,6 +57,19 @@ elif len(sys.argv) == base + 1:
 			db_del.delete_team(sys.argv[3])
 		elif sys.argv[2] == "pilot" :
 			db_del.delete_pilot(sys.argv[3])
+		else:
+			print(errormsg)
+	elif sys.argv[1] == "season" :
+		if sys.argv[2] == "player" :
+			db_seas_player.seasonPlayer(sys.argv[3])
+		elif sys.argv[2] == "stat" :
+			db_seas_stat.seasonStat(sys.argv[3])
+		elif sys.argv[2] == "team" :
+			db_seas_team.seasonTeam(sys.argv[3])
+		else:
+			print(errormsg)
+			
+	
 	else: 
 		print(errormsg)
 		
@@ -78,6 +95,8 @@ elif len(sys.argv) == base + 2:
 			db_add.add_team(sys.argv[3], sys.argv[4])
 		else:
 			print(errormsg)
+	else:
+		print(errormsg)
 		
 elif len(sys.argv) == base + 3:
 	if sys.argv[1] == "add" :
